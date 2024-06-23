6300b84c23942e3fed952f4149ecb1053b57ab87
Dexter Dykes <dexterdykestimothy@gmail.com>
2024-06-23 15:54:19 +0800

    Little update

--- app/src/main/java/com/androidalliance/mynilam/ui/screen/mainfeatures/home/HomeScreen.kt
+++ app/src/main/java/com/androidalliance/mynilam/ui/screen/mainfeatures/home/HomeScreen.kt
@@ -2,25 +2,36 @@
 
 package com.androidalliance.mynilam.ui.screen.mainfeatures.home
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
+import com.androidalliance.mynilam.R
 import com.androidalliance.mynilam.navigation.MainScreen
 import com.androidalliance.mynilam.ui.screen.auths.viewmodel.UserViewModel
 
@@ -41,12 +52,26 @@ fun HomeScreen(
         }
     }
 
+    TopAppBar(
+        title = { Text("MyNilam",
+            color = Color.Black,
+            style = MaterialTheme.typography.titleLarge,
+            fontWeight = FontWeight.Black
+            ) },
+        colors = TopAppBarDefaults.topAppBarColors(containerColor = MaterialTheme.colorScheme.primaryContainer,)
+    )
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text(text = "Home Screen")
+        Spacer(modifier = Modifier.height(20.dp))
+        Image(painter = painterResource(R.drawable.mobile_phone_nilam),
+            contentDescription = "NILAM",
+            modifier = Modifier
+                .size(150.dp)
+                .clip(CircleShape)
+        )
         Spacer(modifier = Modifier.height(20.dp))
         Card(
             modifier = Modifier
--- app/src/main/java/com/androidalliance/mynilam/ui/screen/mainfeatures/profile/ProfileScreen.kt
+++ app/src/main/java/com/androidalliance/mynilam/ui/screen/mainfeatures/profile/ProfileScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
@@ -17,6 +18,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
@@ -52,7 +54,8 @@ fun ProfileScreen(
             horizontalArrangement = Arrangement.SpaceBetween,
             modifier = Modifier.fillMaxWidth()
         ){
-            Text(text = "My Profile")
+            Text(text = "My Profile",style = MaterialTheme.typography.titleLarge,
+                fontWeight = FontWeight.Black)
             Button(
                 onClick = { navController.navigate(ProfilePage.EditForm.route + "/$userId") }
             ) {
